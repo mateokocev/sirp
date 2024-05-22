b726a89d4628b4ac65ffae4b2eb1a10abc814116
mateokocev <mkocev@student.unipu.hr>
2024-05-23 01:03:49 +0200

    dovrsena prezentacija i poboljsane funckijonalnosti u py programu

--- main.py
+++ main.py
@@ -6,16 +6,41 @@ from datetime import datetime
 CSV_FILE_PATH = "Melbourne_housing.csv"
 df = pd.read_csv(CSV_FILE_PATH, delimiter=',')
 
-print("CSV size before: ", df.shape)
+print("Veličina skupa (broj redaka, broj stupaca):", df.shape)
 
-df = df.dropna()
-print("CSV size after: ", df.shape)
-print(df.head())
+print("Nazivi stupaca:", df.columns.tolist())
 
-df20 = df.sample(frac=0.2, random_state=1)
-df = df.drop(df20.index)
-print("CSV size 80: ", df.shape)
-print("CSV size 20: ", df20.shape)
+print("Broj nedostajućih vrijednosti po stupcu:\n", df.isna().sum())
 
-df.to_csv("Melbourne_housing_PROCESSED.csv", index=False) # Spremanje predprocesiranog skupa podataka u novu CSV datoteku
-df20.to_csv("Melbourne_housing_PROCESSED_20.csv", index=False) # Spremanje 20% skupa podataka u novu CSV datoteku
+for column in df.columns:
+    print(f"Jedinstvene vrijednosti u stupcu {column}: {df[column].nunique()}")
+
+print("Tipovi podataka po stupcu:\n", df.dtypes)
+
+for column in df.columns:
+    print(f"Frekvencije vrijednosti u stupcu {column}:\n", df[column].value_counts())
+
+print("\nAnaliza skupa podataka:")
+
+print("Je li je skup podataka dovoljno velik?", df.shape[0] > 1000)
+
+print("Je li skup ima dovoljno različite podatke?", df.nunique().sum() > df.shape[0])
+
+missing_values = df.isna().sum().sum()
+print("Je li skup ima puno nedostajućih vrijednosti?", missing_values > (df.shape[0] * df.shape[1] * 0.1))
+
+
+
+#print("CSV size before: ", df.shape)
+
+#df = df.dropna()
+#print("CSV size after: ", df.shape)
+#print(df.head())
+
+#df20 = df.sample(frac=0.2, random_state=1)
+#df = df.drop(df20.index)
+#print("CSV size 80: ", df.shape)
+#print("CSV size 20: ", df20.shape)
+
+#df.to_csv("Melbourne_housing_PROCESSED.csv", index=False) # Spremanje predprocesiranog skupa podataka u novu CSV datoteku
+#df20.to_csv("Melbourne_housing_PROCESSED_20.csv", index=False) # Spremanje 20% skupa podataka u novu CSV datoteku
