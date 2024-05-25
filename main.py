9a8ed15c9acaea8ba28fb6d850074e85fc1ec8dd
mateokocev <mkocev@student.unipu.hr>
2024-05-25 17:42:50 +0200

    update

--- main.py
+++ main.py
@@ -17,19 +17,12 @@ for column in df.columns:
 
 print("Tipovi podataka po stupcu:\n", df.dtypes)
 
-for column in df.columns:
-    print(f"Frekvencije vrijednosti u stupcu {column}:\n", df[column].value_counts())
-
 print("\nAnaliza skupa podataka:")
 
 print("Je li je skup podataka dovoljno velik?", df.shape[0] > 1000)
 
-print("Je li skup ima dovoljno različite podatke?", df.nunique().sum() > df.shape[0])
-
 missing_values = df.isna().sum().sum()
 print("Je li skup ima puno nedostajućih vrijednosti?", missing_values > (df.shape[0] * df.shape[1] * 0.1))
-
-
 
 #print("CSV size before: ", df.shape)
 
@@ -42,5 +35,5 @@ print("Je li skup ima puno nedostajućih vrijednosti?", missing_values > (df.sha
 #print("CSV size 80: ", df.shape)
 #print("CSV size 20: ", df20.shape)
 
-#df.to_csv("Melbourne_housing_PROCESSED.csv", index=False) # Spremanje predprocesiranog skupa podataka u novu CSV datoteku
-#df20.to_csv("Melbourne_housing_PROCESSED_20.csv", index=False) # Spremanje 20% skupa podataka u novu CSV datoteku
+#df.to_csv("Melbourne_housing_PROCESSED.csv", index=False)
+#df20.to_csv("Melbourne_housing_PROCESSED_20.csv", index=False)
